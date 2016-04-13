fee8b206f3d702082a3f6bd20dc4d5beefea2eff
Alejandro Serradilla <alexserradillarufino@gmail.com>
2016-04-14 01:38:02 +0200

    Added stock methods

--- lib/product.rb
+++ lib/product.rb
@@ -3,24 +3,41 @@ class Product
 
         @@products = []
 
+        # Initializes a product
         def initialize(options={})
                 @title = options[:title]
+                @price= options[:price]
+                @stock = options[:stock]
                 add_to_products
         end
 
+        # Returns all the products
         def self.all
                 @@products
         end
 
-        def self.in_stock
-                @@products.select { |product| product[:stock] > 0 }
+        # Finds a product by its name
+        def self.find_by_title(title)
+                @@products.each do |product|
+                        if product.title == title
+                                return product
+                        end
+                end
         end
 
+        # Returns an array of the products that are in stock
+        def self.in_stock
+                @@products.select { |product| product.stock > 0 }
+        end
+
+        # Return wether a product is in stock or not
         def in_stock?
+                Product.in_stock.include?(self)
         end
 
         private
 
+        # Adds a product to the array if it doesn't exists already
         def add_to_products
                 @@products.each do |product|
                         if product.title == @title
